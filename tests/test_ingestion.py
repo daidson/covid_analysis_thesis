760e8672b75038a03cbe188fcfb02150eb9364f5
Daidson Alves <daidson.alves@gmail.com>
2022-06-01 01:17:31 -0300

    adding api tests for ingestion

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,12 +1,27 @@
+from importlib.resources import path
+import re
+import pytest
 import requests
 import json
-import pytest
-from ingestion.ingest import DataIngestion
+from ingestion.ingest import DataIngestion as dai
 
 
-@pytest
-def should_test_api_connection_status():    
-    test_url = ""
-    requested_data_status = DataIngestion.get_api_data_status(test_url)
+# @pytest.mark.skip
+def test_api_connection_status():    
+    test_url = "https://httpbin.org/ip"
+    requested_data_status = dai.get_api_data_status(test_url)
 
     assert requested_data_status == 200
+
+# TDD -> Test Driven Design
+# BDD -> Behavior Driven Design
+
+# @pytest.mark.skip
+def test_data_ingestion():
+    test_url = "https://httpbin.org/ip"
+    request_data = dai.get_data_from_pysus(test_url)
+
+    path_to_json = 'tests\mock_data.json'
+    mock_data = json.loads(open(path_to_json).read())
+    
+    assert request_data == mock_data
