177fe2046f56bc2dc961582cebdcb46747e6aa14
Daidson Alves <daidson.alves@gmail.com>
2022-11-12 22:26:34 -0300

    creating modeling method

---
+++ modeling/__init__.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+SPARK = SparkSession.builder.master('local') \
+                            .appName("Data Ingestion") \
+                            .config('spark.submit.deployMode', 'client') \
+                            .config('spark.driver.memory','16g') \
+                            .config('spark.executor.memory','4g') \
+                            .config('spark.executor.cores', 4) \
+                            .config('spark.sql.debug.maxToStringFields', 1000) \
+                            .config("spark.sql.execution.arrow.pyspark.enabled", True) \
+                            .config("spark.sql.repl.eagerEval.enabled", True) \
+                            .config("spark.sql.shuffle.partitions", 1000) \
+                            .enableHiveSupport() \
+                            .getOrCreate()
--- modeling/model_data.py
+++ modeling/model_data.py
@@ -1,21 +1,22 @@
-import json
 from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType, StructField, StringType, BooleanType, ArrayType
-from elasticsearch import Elasticsearch
 import datetime
 
 import os 
 from dotenv import load_dotenv
 from pathlib import Path
 
-from ingestion import SPARK
+from modeling import SPARK
 
 load_dotenv(dotenv_path=Path('.env'))
 
-class EsusApiIngestion():
-    """A class to ingest data from TABNET/DATASUS/SUS"""
+class DataModeling():
+    """
+    A class to model data from TABNET/DATASUS/SUS
+    This class allows a user to model data in order to consume it.
+    Its  methods can be decoupled from their use case and used globally."""
 
-    def define_ingestion_schema(self) -> list:
+    def define_schema(self) -> list:
         """
         Function to map Covid Data schema from SUS-Tabnet
         This function returns a Type argument with a schema list of columns
@@ -87,88 +88,7 @@ class EsusApiIngestion():
 
         return schema
     
-
-    def ingest_covid_dataframe(self, spark: SPARK, schema: list, uf: str) -> DataFrame:
-        """
-        Function to ingest all Covid data from SUS-Tabnet using Pyspark
-
-        :param spark: Spark configuration session. Please refer to spark docs when building one.
-        :param uf: Brazilian state reference (there are 27 different states)
-        :param url: ESUS Elasticsearch connection string
-        
-        """
-        self.UF = uf.lower()
-
-        es = Elasticsearch([os.getenv('ESUS_URL')], send_get_body_as="POST")
-
-        query = {"match_all": {}}
-
-        index_to_access = os.getenv('ESUS_DATABASE') + self.UF
-
-        page = es.search(
-            index = index_to_access,
-            doc_type = None,
-            scroll = '5m',
-            search_type = 'query_then_fetch',
-            size = 10000,
-            query = query
-        )
-        sid = page['_scroll_id']
-        scroll_size = page['hits']['total']["value"]
-        
-        data = []
-        while(scroll_size > 0):
-            page = es.scroll(scroll_id = sid, scroll = '5m')
-            sid = page['_scroll_id']
-            scroll_size = len(page['hits']['hits'])
-            for hit in page['hits']['hits']:
-                data.append(hit["_source"])
-
-        dataframe = spark.createDataFrame(data=data, schema=schema)
-        
-        return dataframe
-
-    def ingest_covid_data_json(self, uf: str) -> list:
-        """
-        Function to ingest all Covid data from SUS-Tabnet using Pyspark
-
-        :param spark: Spark configuration session. Please refer to spark docs when building one.
-        :param uf: Brazilian state reference (there are 27 different states)
-        :param url: ESUS Elasticsearch connection string
-        
-        """
-        self.UF = uf.lower()
-
-        es = Elasticsearch([os.getenv('ESUS_URL')], send_get_body_as="POST", 
-                            timeout=60, max_retries=10, retry_on_timeout=True)
-
-        query = {"match_all": {}}
-
-        index_to_access = os.getenv('ESUS_DATABASE') + self.UF
-
-        page = es.search(
-            index = index_to_access,
-            doc_type = None,
-            scroll = '5m',
-            search_type = 'query_then_fetch',
-            size = 10000,
-            query = query,
-            request_timeout=60
-        )
-        sid = page['_scroll_id']
-        scroll_size = page['hits']['total']["value"]
-        
-        data = []
-        while(scroll_size > 0):
-            page = es.scroll(scroll_id = sid, scroll = '5m')
-            sid = page['_scroll_id']
-            scroll_size = len(page['hits']['hits'])
-            for hit in page['hits']['hits']:
-                data.append(hit["_source"])
-
-        return data
-
-    def ingest_sample_dataframe(self, spark: SPARK, schema: list, uf: str) -> DataFrame:
+    def ingest_sample_dataframe(self, spark: SPARK, dataframe: DataFrame) -> DataFrame:
         """
         Function to ingest a Covid data sample from SUS-Tabnet using Pyspark
         This function returns only 10 thousand registers from Tabnet
@@ -177,90 +97,60 @@ class EsusApiIngestion():
         :param uf: Brazilian state reference (there are 27 different states)
         :param url: ESUS Elasticsearch connection string
         """
-        self.UF = uf.lower()
-
-        es = Elasticsearch([os.getenv('ESUS_URL')], send_get_body_as="POST")
-
-        query = {"match_all": {}}
-
-        index_to_access = os.getenv('ESUS_DATABASE') + self.UF
-
-        results = es.search(query=query,
-                            size=10000, 
-                            request_timeout=60, 
-                            index=index_to_access, 
-                            filter_path=['hits.hits._source'])
-        final_results = results['hits']['hits']
         
-        data = []
-        for result in final_results:
-            data.append(result["_source"])
-
-        dataframe = spark.createDataFrame(data=data, schema=schema)
         
         return dataframe
-
-    def ingest_sample_data_json(self, uf: str) -> list:
-        """
-        Function to ingest a Covid data sample from SUS-Tabnet using Pyspark
-        This function returns only 10 thousand registers from Tabnet
-
-        :param spark: Spark configuration session. Please refer to spark docs when building one.
-        :param uf: Brazilian state reference (there are 27 different states)
-        :param url: ESUS Elasticsearch connection string
-        """
-        self.UF = uf.lower()
-
-        es = Elasticsearch([os.getenv('ESUS_URL')], send_get_body_as="POST")
-
-        query = {"match_all": {}}
-
-        index_to_access = os.getenv('ESUS_DATABASE') + self.UF
-
-        results = es.search(query=query,
-                            size=10000, 
-                            request_timeout=60, 
-                            index=index_to_access, 
-                            filter_path=['hits.hits._source'])
-        final_results = results['hits']['hits']
-        
-        data = []
-        for result in final_results:
-            data.append(result["_source"])
-
-        return data    
     
-    def write_ingested_dataframe(self, dataframe: DataFrame, uf: str) -> None:
+    def get_last_testing_data(self, dataframe: DataFrame) -> DataFrame:
+        """
+        Function to get data from the last COVID test a person has taken.
+        It also drops the 'testes' column as it returns data from all tests a person has taken.
+        This function returns a dataframe type.
+
+        :param dataframe: Input dataframe to have data changed
+        """
+        
+        dataframe = dataframe.withColumn("codigoEstadoTeste", dataframe.testes[0].codigoEstadoTeste) \
+            .withColumn("codigoFabricanteTeste",  dataframe.testes[0].codigoFabricanteTeste) \
+            .withColumn("codigoResultadoTeste",  dataframe.testes[0].codigoResultadoTeste) \
+            .withColumn("codigoTipoTeste",  dataframe.testes[0].codigoTipoTeste) \
+            .withColumn("dataColetaTeste",  dataframe.testes[0].dataColetaTeste.iso) \
+            .withColumn("estadoTeste",  dataframe.testes[0].estadoTeste) \
+            .withColumn("fabricanteTeste",  dataframe.testes[0].fabricanteTeste) \
+            .withColumn("loteTeste",  dataframe.testes[0].loteTeste) \
+            .withColumn("resultadoTeste",  dataframe.testes[0].resultadoTeste) \
+            .withColumn("tipoTeste",  dataframe.testes[0].tipoTeste)
+        
+        dataframe = dataframe.drop(dataframe.testes)
+        
+        return dataframe
+    
+    def read_json_into_dataframe(self, spark: SPARK, path: str) -> DataFrame:
+        """
+        Function to read json and make it a dataframe using Pyspark.
+        This function returns a dataframe type.
+
+        :param spark: Spark configuration session. Please refer to spark docs when building one.
+        :param path: Desired Json path to be read.
+        """
+        dataframe = spark.read.json(path)
+        
+        return dataframe
+
+    
+    def write_modeled_dataframe(self, dataframe: DataFrame, uf: str) -> None:
         """
         Function to save dataframe in parquet
         """
-        input_df = dataframe
         today = datetime.datetime.now()
         dt = today.strftime("%d_%m_%Y_%H_%M_%S")
         output_name = 'esus_data_' + uf + '_' + dt + '.parquet'
-        output_dir = 'ingested_data'
+        output_dir = 'modeled_data'
 
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
 
-        input_df.write.parquet(f"{output_dir}/{output_name}")
-
-        return print("Dataframe saved to desired path")
-
-    def write_ingested_json(self, data: list, uf: str) -> None:
-        """
-        Function to save dataframe in parquet
-        """
-        today = datetime.datetime.now()
-        dt = today.strftime("%d_%m_%Y_%H_%M_%S")
-        output_name = 'esus_data_' + uf + '_' + dt + '.json'
-        output_dir = 'ingested_data'
-
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
-
-        with open(f"{output_dir}/{output_name}", 'w', encoding='utf-8') as fp:
-            json.dump(data, fp, ensure_ascii=False)
+        dataframe.write.parquet(f"{output_dir}/{output_name}")
 
         return print("Dataframe saved to desired path")
 
