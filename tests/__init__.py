9839e190a6634803d8d5d48134ad7f396bf1b475
Daidson Alves <daidson.alves@gmail.com>
2022-11-21 07:50:06 -0300

    setting tuning parameters

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,9 @@ SPARK = SparkSession.builder.master('local') \
                             .appName("Test Class") \
                             .config('spark.submit.deployMode', 'client') \
                             .config('spark.driver.memory','16g') \
-                            .config('spark.executor.memory','16g') \
+                            .config('spark.executor.memory','4g') \
+                            .config('spark.memory.offHeap.enabled', True) \
+                            .config('spark.memory.offHeap.size','16g') \
                             .config('spark.executor.cores', 4) \
                             .config('spark.sql.debug.maxToStringFields', 1000) \
                             .config("spark.sql.execution.arrow.pyspark.enabled", True) \
